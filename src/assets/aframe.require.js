192bd2260dc1416bd8e1e77e47fde5bafac1f937
Leon van Kammen <leonvankammen@gmail.com>
2024-05-24 14:37:21 +0000

    feat/hand-menu: work in progress [might break]

--- src/assets/aframe.require.js
+++ src/assets/aframe.require.js
@@ -1,10 +1,11 @@
 // usage: 
 //
+// await AFRAME.utils.require( this.dependencies )               (*) autoload missing components
 // await AFRAME.utils.require( this.el.getAttributeNames() )     (*) autoload missing components
 // await AFRAME.utils.require({foo: "https://foo.com/aframe/components/foo.js"},this)
 // await AFRAME.utils.require(["./app/foo.js","foo.css"],this)
 // 
-// (*) = prefix baseURL by setting AFRAME.utils.require.baseURL = './com/' e.g.
+// (*) = prefixes baseURL AFRAME.utils.require.baseURL ('./com/' e.g.)
 //
 AFRAME.utils.require = function(arr_or_obj,opts){
   opts            = opts || {}
@@ -23,6 +24,7 @@ AFRAME.utils.require = function(arr_or_obj,opts){
 
   packagesArr.map( (package) => {
     try{
+      package = package.match(/\./) ? package : AFRAME.utils.require.baseURL+package+".js"
       let id = Object.keys(arr_or_obj)[i]
       if( id == i ) id = parseURI(package).component 
       // prevent duplicate requests
@@ -35,9 +37,9 @@ AFRAME.utils.require = function(arr_or_obj,opts){
           switch(type){
             case "js":  let script = document.createElement("script")
                         script.id  = id
-                        script.src = package
-                        script.onload  = () => resolve(id)
+                        script.onload  = () => setTimeout( () => resolve(id), 50 )
                         script.onerror = (e) => reject(e)
+                        script.src = package
                         document.head.appendChild(script)
                         break;
             case "css": let link = document.createElement("link")
